727da7a22be234fc259bfa1c43198b5da6d5821b
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-23 19:24:19 +0300

    added mamasearcher

---
+++ MamaSearcher/IMamaSearcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaSearcher
+{
+    public interface IMamaSearcher
+    {
+        void Subscribe(string pattern, Action<int, string> actionToPerform);
+        void PerfomSearch(string content);
+    }
+}
---
+++ MamaSearcher/MamaSearcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaSearcher
+{
+    class MamaSearcher : IMamaSearcher
+    {
+        public Dictionary<string, Action<int, string>> PatternActionRunner{get; set ;}
+        public MamaSearcher()
+        {
+            PatternActionRunner = new Dictionary<string, Action<int, string>>();
+        }
+        public void PerfomSearch(string content)
+        {
+            if (content == null)
+            {
+                Console.WriteLine("cant search pattern on null string");
+                return;
+            }
+            foreach (var stringPattern in PatternActionRunner.Keys)
+            {
+                if (content.Contains(stringPattern))
+                {
+                    PatternActionRunner[stringPattern](content.IndexOf(stringPattern), stringPattern);
+                }
+            }
+        }
+
+        public void Subscribe(string pattern, Action<int, string> actionToPerform)
+        {
+            if (pattern == null)
+            {
+                Console.WriteLine("Cant get null string ad pattern");
+                return;
+            }
+            if (!PatternActionRunner.ContainsKey(pattern))
+            {
+                PatternActionRunner.Add(pattern, actionToPerform);
+            }
+            else
+            {
+                PatternActionRunner[pattern] += actionToPerform;
+            }
+            
+
+        }
+    }
+}
